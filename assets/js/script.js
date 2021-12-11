b29b631777a10fd5c5557c03e8f958d89201b711
Brad Kuhne <bjkuhne@aol.com>
2021-12-11 12:18:00 -0600

    looping thru questions and starting answers function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var buttonEl = document.querySelector("#start");
 var questionsEl = document.querySelector("#questions-to-ask");
+var answersEl = document.querySelector("answers");
 
-const questions = ["What color is the most prolific lure color?", "All other things being eqaul, what time of day is the best time to catch fish?", "What is the name of the style for rigging a plastic worm in-line on an offset hook with a weight directly on top of the hook?", "Do you generall want to use stronger or thinner line when fishing around structure such as trees or moss?"];
+const questions = ["What color is the most prolific lure color?", "All other things being eqaul, what time of day is the best time to catch fish?", "What is the name of the style for rigging a plastic worm in-line on an offset hook with a weight directly on top of the hook?", "Should you use stronger or lighter line when fishing around structure such as trees or moss?"];
 const quest1Answers = ["Blue", "Red", "Purple", "Chartreuse"];
 var quest1Correct = quest1Answers[3];
 console.log (quest1Correct);
@@ -15,14 +16,28 @@ const quest4Answers = ["Lighter", "Stronger", "Doesn't matter", "Don't fish near
 var quest4Correct = quest4Answers[1];
 console.log (quest4Correct);
 
+var questionCount = 0
 
 
 
 
-
-var startQuiz = function() {
+startQuiz = function() {
     console.log ("button was clicked");
-    questionsEl.textContent = questions[0];
-    };
+    // for (let i = 0; i < questions.length; i++) {
+    if (questionCount < questions.length){
+        console.log ("question count: " + questionCount + " Questions length: " + questions.length);
+        questionsEl.textContent = questions[questionCount];
+        presentAnswers();
+        buttonEl.addEventListener("click", startQuiz);
+        questionCount++;
+    }   else {
+            console.log ("need to show all done text");
+    } 
+    //};
+}
+
+function presentAnswers() {
+    console.log ("in present answers function");
+}
 
 buttonEl.addEventListener("click", startQuiz);
