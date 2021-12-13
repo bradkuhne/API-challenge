d384d5f8597c597e9a9c8192cbf1eba091c59a0b
Brad Kuhne <bjkuhne@aol.com>
2021-12-12 21:01:17 -0600

    added timer and messagee for end game screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ var buttonEl = document.querySelector("#start");
 var questionsEl = document.querySelector("#questions-to-ask");
 var answersList = document.querySelector("#answers");
 //var answerBtn1 = document.querySelector("0");
+var questionCounter = 0;
+
+var timerEl = document.getElementById('timeCheck');
 
 
 const questions = ["What color is the most prolific fishing lure color?", "All other things being eqaul, what time of day is the best time to catch fish?", "What is the name of the style for rigging a plastic worm in-line on an offset hook with a weight directly on top of the hook?", "Should you use stronger or lighter line when fishing around structure such as trees or moss?"];
@@ -18,82 +21,199 @@ const quest4Answers = ["Lighter", "Stronger", "Doesn't matter", "Don't fish near
 var quest4Correct = quest4Answers[1];
 // console.log (quest4Correct);
 
+var remTime = 0
+
 var screenAnswers = []
 
 var questionCount = 0
 
+// Timer that counts down from 75
+function countdown() {
+    console.log ("inside countdown function");
+    var timeLeft = 10;
+    console.log ("First time left = " + timeLeft);
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      console.log ("inside timeInterval function.  Time left: " + timeLeft);
+        // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+         console.log ("Time left = " + timeLeft);
+         // Set the `textContent` of `timerEl` to show the remaining seconds
+         timerEl.textContent = timeLeft;
+         console.log ("This should be shown in timer: " + timerEl.textContent);
+    //     // Decrement `timeLeft` by 1
+    timeLeft--;
+       } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '';
+
+         // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+         // Call the `displayMessage()` function
+         console.log ("about to go to end game function");
+         endGame();
+     }
+    }, 1000);
+  }
+
+
+
+
+
+
 startQuiz = function() {
     console.log ("button was clicked");
+    console.log ("question count: " + questionCount);
+    console.log ("question count: " + questionCount + " Questions length: " + questions.length);
+    countdown();
     if (questionCount < questions.length){
-        console.log ("question count: " + questionCount + " Questions length: " + questions.length);
         questionsEl.textContent = questions[questionCount];
         removeText();
         presentAnswers();
-        buttonEl.addEventListener("click", startQuiz);
+       // buttonEl.addEventListener("click", startQuiz);
         questionCount++;
+        console.log("After adding one to question Count it equals: " + questionCount);
     }   else {
             console.log ("need to show all done text");
     } 
 };
 
-function createAnswerHandler() {
-    // create container to hold elements
-    var actionContainerEl = document.createElement("div");
-    actionContainerEl.className = "answers";
-    // loop through the four answers and add to screen
+function createAnswer1() {
     for (let x = 0; x < 4; x++) {
-        // var answersEl = document.createElement("li");
         var answersEl = document.createElement("button");
         answersEl.type = "button";
         answersEl.className = "answersClass";
-        answersEl.textContent = "quest1Answers[x]";
         answersEl.innerText = quest1Answers[x];
-        answersEl.id = x;
         answersList.appendChild(answersEl);
-        console.log ("This is the action container El: " + actionContainerEl + "answersListEl: " + answersEl);
     };
-
-    // console.log ("This is the value of answerBtn1: " + answerBtn1);
-
-    answersList.addEventListener("click", judgeAnswer, true);
-    
-
-    //TRIAL CODE BELOW TRYING TO FIGURE OUT HOW TO GET TO THE ANSWER OF WHAT BUTTON IS PUSHED
-    // for (let x = 0; x < 4; x++) {
-    //     var containerEl = document.createElement("li");
-    //     containerEl.className = "answers"
-    //     screenAnswers[x] = document.createElement("button");
-    //     screenAnswers[x].className = "button";
-    //     screenAnswers[x].textContent = "quest1Answers[x]";
-    //     screenAnswers[x].innerText = quest1Answers[x];
-    //     screenAnswers[x].id = x;
-    //     containerEl.appendChild(screenAnswers[x]);
-    //     console.log ("This is the containerEl: " + containerEl + "screenAnswers[1]: " + screenAnswers[1]);
-    // };
-    // screenAnswers[1].addEventListener("click", judgeAnswer, true);
-    // answersClass.addEventListener("click", judgeAnswer, true);
-    
-    // const element = target 
-    // console.log (element.id)
+    if (questionCounter < 4) {
+        answersList.addEventListener("click", judgeAnswer, true);
+    };
 };
+function createAnswer2() {
+    var answerId = document.getElementById("answers");
+    for (let i = 0; i < 4; i++) {
+        console.log ("i equals: " + i)
+        answerId.removeChild(answerId.childNodes[0]);
+    };
+    // var answerId = document.getElementById("answers");
+    //     answerId.removeChild(answerId.childNodes[0]);
+    //     answerId.removeChild(answerId.childNodes[1]);
+    //     answerId.removeChild(answerId.childNodes[2]);
+    //     answerId.removeChild(answerId.childNodes[3]);
+    for (let x = 0; x < 4; x++) {
+        var answersEl = document.createElement("button");
+        answersEl.type = "button";
+        answersEl.className = "answersClass";
+        answersEl.innerText = quest2Answers[x];
+        answersList.appendChild(answersEl);
+    };
+    if (questionCounter < 4) {
+        answersList.addEventListener("click", judgeAnswer, true);
+    };
+};
+function createAnswer3() {
+    var answerId = document.getElementById("answers");
+    for (let i = 0; i < 5; i++) {
+        console.log ("i equals: " + i)
+        answerId.removeChild(answerId.childNodes[0]);
+    };
+    // var answerId = document.getElementById("answers");
+    //     answerId.removeChild(answerId.childNodes[0]);
+    //     answerId.removeChild(answerId.childNodes[1]);
+    //     answerId.removeChild(answerId.childNodes[2]);
+    //     answerId.removeChild(answerId.childNodes[3]);
+    for (let x = 0; x < 4; x++) {
+        var answersEl = document.createElement("button");
+        answersEl.type = "button";
+        answersEl.className = "answersClass";
+        answersEl.innerText = quest3Answers[x];
+        answersList.appendChild(answersEl);
+    };
+    if (questionCounter < 4) {
+        answersList.addEventListener("click", judgeAnswer, true);
+    };
+};
+function createAnswer4() {
+    var answerId = document.getElementById("answers");
+    for (let i = 0; i < 5; i++) {
+        console.log ("i equals: " + i)
+        answerId.removeChild(answerId.childNodes[0]);
+    };
+    // var answerId = document.getElementById("answers");
+    //     answerId.removeChild(answerId.childNodes[0]);
+    //     answerId.removeChild(answerId.childNodes[1]);
+    //     answerId.removeChild(answerId.childNodes[2]);
+    //     answerId.removeChild(answerId.childNodes[3]);
+    for (let x = 0; x < 4; x++) {
+        var answersEl = document.createElement("button");
+        answersEl.type = "button";
+        answersEl.className = "answersClass";
+        answersEl.innerText = quest4Answers[x];
+        answersList.appendChild(answersEl);
+    };
+    if (questionCounter < 4) {
+        answersList.addEventListener("click", judgeAnswer, true);
+    };
+};
+
+
 judgeAnswer = function () {
-    var answerName = document.querySelector("button").getAttribute("id");
-    console.log ("answer button was clicked.  I need to determine which one.");
-    console.log ("button value: " + answerName);
-    if (answerName = "0") {
+    
+    
+
+    // var btn1Chosen = document.getElementById("2");
+    // console.log ("this is the value of btn1Chosen " + btn1Chosen.id);
+    // if (btn1Chosen) {
+    //     console.log ("button 1 was chosen");
+    // }
+    if (questionCounter < 4 ) {
+        var answerName = document.querySelector("button").getAttribute("id");
+        console.log ("answer button was clicked.  I need to determine which one.");
+        console.log ("button value: " + answerName);
         console.log ("inside answer name");
+        questionCounter++
+        startQuiz();
+        // var feedback = document.createElement("div");
         var answerFeedback = document.createElement("p");
         answerFeedback.textContent = "Correct!";
         answerFeedback.className = "pWithBorder";
+        // feedback.appendChild(answerFeedback);
         answersList.appendChild(answerFeedback);
     };
-    // for (let y = 0; y < 3; y++) {
-        
-    // }
-
-    
+    console.log ("*** THIS is the questionCounter: " + questionCounter);
+    if (questionCounter == 4){
+        console.log ("need to call last end-game function " + questionCounter);
+        endGame();
+    };
+      
 }
 
+function endGame() {
+    var answerId = document.getElementById("answers");
+    console.log ("THIS IS THE END GAME function");
+    questionsEl.textContent = ("All done!");
+    // var answerId = document.getElementById("answers");
+    console.log ("this is the number of questions we are on: " + questionCounter)
+    if (questionCounter > 0){
+        for (let i = 0; i < 5; i++) {
+            console.log ("i equals: " + i)
+            answerId.removeChild(answerId.childNodes[0]);
+        };
+    } else {
+            for (let i = 0; i < 4; i++) {
+                console.log ("i equals: " + i)
+                answerId.removeChild(answerId.childNodes[0]);
+            };
+        }
+    var endGameEl = document.createElement("div");
+    endGameEl.className = "endGame";
+    endGameEl.innerHTML =
+    // "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    "<p>'HELLO WORLD'</p>"; 
+  answerId.appendChild(endGameEl);
+};
 
 function removeText() {
     var textToRemove = document.getElementById("openingText");
@@ -111,20 +231,19 @@ function presentAnswers() {
     switch (questionCount) {
         case 0:
             console.log ("question 1 answers: " + quest1Answers[0]);
-            // answersEl.textContent = quest1Answers[0];
-            createAnswerHandler();            
+            createAnswer1();            
             break;
         case 1:
             console.log ("question 2 answers: " + quest2Answers[0]);
-            answersEl.textContent = quest2Answers[0];
+            createAnswer2();
             break;
         case 2:
             console.log ("question 3 answers: " + quest3Answers[0]);
-            answersEl.textContent = quest3Answers[0];
+            createAnswer3();
             break;
         case 3:
             console.log ("question 4 answers: " + quest4Answers[0]);
-            answersEl.textContent = quest4Answers[0];
+            createAnswer4();
             break;
         default:
             console.log ("something went wrong");
